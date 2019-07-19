38411dc5803e54ba041ed00772e18f573cec61b3
Clément Habinshuti <hello@habbes.xyz>
2019-07-19 16:13:07 +0300

    Placeholders for basic layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Habbes Spreadsheet
+      <header className="App-header">
+        header
+      </header>
+      <div class="App-formula-bar">
+        formula bar
+      </div>
+      <div class="Grid">
+        grid
+      </div>
     </div>
   );
 }
