d04c422810cbff664268e9d14f8fbfc2d6191ddf
Clément Habinshuti <hello@habbes.xyz>
2019-07-20 18:35:43 +0300

    Refactor parser tests

--- src/core/eval/tests/parser.test.js
+++ src/core/eval/tests/parser.test.js
@@ -1,8 +1,6 @@
 import { Parser } from '../parser';
 import { number, string, cell, symbol, identifier } from '../token';
-import { CellRangeNode, StringNode, NumberNode, CellNode, FunctionCallNode } from '../parse-tree';
-
-
+import { NumberNode, StringNode, CellNode, CellRangeNode, FunctionCallNode } from '../parse-tree';
 
 describe('Parser', () => {
     /**
@@ -82,54 +80,54 @@ describe('Parser', () => {
         const { testParse, testError } = createParseTestersFor(() => parser.parseFunctionCall());
 
         it('should parse call with no args', () => {
-            parser.init([identifier('sum'), symbol('('), symbol(')')]);
-            const node = parser.parseFunctionCall();
-            expect(node).toEqual(new FunctionCallNode('sum', []));
-            expect(parser.tokens).toEqual([]);
+            testParse([identifier('sum'), symbol('('), symbol(')')], new FunctionCallNode('sum', []), []);
         });
         it('should parse call with one arg', () => {
-            parser.init([identifier('exp'), symbol('('), cell('A3'), symbol(')'), symbol(',')]);
-            const node = parser.parseFunctionCall();
-            expect(node).toEqual(new FunctionCallNode('exp', [new CellNode('A3')]));
-            expect(parser.tokens).toEqual([symbol(',')]);
+            testParse([identifier('exp'), symbol('('), cell('A3'), symbol(')'), symbol(',')],
+            new FunctionCallNode('exp', [new CellNode('A3')]), [symbol(',')]);
         });
         it('should parse call with multiple args', () => {
-            parser.init([identifier('func'), symbol('('),
-                number('23.4'), symbol(','),
-                cell('B2'), symbol(','),
-                string('"test"'), symbol(','),
-                cell('C10'), symbol(':'), cell('C15'),
-                symbol(')'), symbol(',')]);
-            const node = parser.parseFunctionCall();
-            expect(node).toEqual(new FunctionCallNode('func', [
-                new NumberNode('23.4'), new CellNode('B2'), new StringNode('"test"'), new CellRangeNode('C10', 'C15')
-            ]));
-            expect(parser.tokens).toEqual([symbol(',')]);
+            testParse(
+                [
+                    identifier('func'), symbol('('),
+                    number('23.4'), symbol(','),
+                    cell('B2'), symbol(','),
+                    string('"test"'), symbol(','),
+                    cell('C10'), symbol(':'), cell('C15'),
+                    symbol(')'), symbol(',')
+                ],
+                new FunctionCallNode('func', [
+                    new NumberNode('23.4'), new CellNode('B2'), new StringNode('"test"'), new CellRangeNode('C10', 'C15')
+                ]),
+                [symbol(',')]
+            );
         });
         it('should parse call with args containing other function calls', () => {
-            parser.init([identifier('func'), symbol('('),
-                cell('C4'), symbol(','),
-                identifier('func2'), symbol('('), cell('B4'), symbol(':'), cell('G4'), symbol(')'), symbol(','),
-                identifier('func3'), symbol('('), identifier('func4'), symbol('('), symbol(')'), symbol(')'), symbol(','),
-                number('100'), symbol(','),
-                identifier('func5'), symbol('('), number('25'), symbol(','), string('"s"'), symbol(')'),
-                symbol(')')
-            ]);
-            const node = parser.parseFunctionCall();
-            expect(node).toEqual(new FunctionCallNode('func', [
-                new CellNode('C4'),
-                new FunctionCallNode('func2', [
-                    new CellRangeNode('B4', 'G4')
+            testParse(
+                [
+                    identifier('func'), symbol('('),
+                    cell('C4'), symbol(','),
+                    identifier('func2'), symbol('('), cell('B4'), symbol(':'), cell('G4'), symbol(')'), symbol(','),
+                    identifier('func3'), symbol('('), identifier('func4'), symbol('('), symbol(')'), symbol(')'), symbol(','),
+                    number('100'), symbol(','),
+                    identifier('func5'), symbol('('), number('25'), symbol(','), string('"s"'), symbol(')'),
+                    symbol(')')
+                ],
+                new FunctionCallNode('func', [
+                    new CellNode('C4'),
+                    new FunctionCallNode('func2', [
+                        new CellRangeNode('B4', 'G4')
+                    ]),
+                    new FunctionCallNode('func3', [
+                        new FunctionCallNode('func4', [])
+                    ]),
+                    new NumberNode('100'),
+                    new FunctionCallNode('func5', [
+                        new NumberNode('25'), new StringNode('"s"')
+                    ])
                 ]),
-                new FunctionCallNode('func3', [
-                    new FunctionCallNode('func4', [])
-                ]),
-                new NumberNode('100'),
-                new FunctionCallNode('func5', [
-                    new NumberNode('25'), new StringNode('"s"')
-                ])
-            ]));
-            expect(parser.tokens).toEqual([]);
+                []
+            );
         });
         it('should throw errors on unexpected syntax', () => {
             testError([], /unexpected end of input/i);
