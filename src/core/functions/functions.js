a6cc8b8446fdfe8faf231f1330fa0dee3200aa1a
Clément Habinshuti <hello@habbes.xyz>
2019-07-25 22:25:08 +0300

    Add count function

--- src/core/functions/functions.js
+++ src/core/functions/functions.js
@@ -4,7 +4,10 @@ export const sum = makeNumberReducer('SUM', (a, b) => a + b, 0);
 
 export const product = makeNumberReducer('PRODUCT', (a, b) => a * b, 1);
 
+export const count = makeNumberReducer('COUNT', a => a + 1, 0);
+
 const avgCounter = makeNumberReducer('AVG', ([total, count], curr) => [total + curr, count + 1], [0, 0]);
+
 
 export function avg (...args) {
     const [total, count] = avgCounter(...args);
